da4de1dc2dfaa7523a0f0d02f927f6f17f3475db
Proylan <froilandevera07@gmail.com>
2024-09-23 20:50:14 +0800

    Added pages

---
+++ app/src/main/java/com/example/intelliquiz/Leaderboards.kt
@@ -0,0 +1,32 @@
+package com.example.intelliquiz
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Leaderboards : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_leaderboards)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val HomeButton: Button = findViewById(R.id.homeBtn)
+
+        HomeButton.setOnClickListener {
+            // Create an Intent to start the SecondActivity
+            val intent = Intent(this, Welcome_Page::class.java)
+            startActivity(intent)  // This starts the SecondActivity
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/intelliquiz/Welcome_Page.kt
+++ app/src/main/java/com/example/intelliquiz/Welcome_Page.kt
@@ -1,6 +1,8 @@
         package com.example.intelliquiz
 
+        import android.content.Intent
         import android.os.Bundle
+        import android.widget.Button
         import androidx.activity.enableEdgeToEdge
         import androidx.appcompat.app.AppCompatActivity
         import androidx.core.view.ViewCompat
@@ -16,5 +18,22 @@
                     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
                     insets
                 }
+
+                val enterButton: Button = findViewById(R.id.playBtn)
+                val LeaderBtn: Button = findViewById(R.id.leaderBtn)
+
+                enterButton.setOnClickListener {
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)  // This starts the SecondActivity
+                }
+
+                LeaderBtn.setOnClickListener {
+                    val intent  = Intent(this, Leaderboards::class.java)
+                    startActivity(intent)
+                }
+
+
+
             }
         }
