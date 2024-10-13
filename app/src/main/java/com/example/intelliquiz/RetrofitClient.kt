371df4e8ac8cbbf1dd5a720904b7ccd67398e49e
Proylan <froilandevera07@gmail.com>
2024-10-13 15:32:22 +0800

    remodeled version

--- app/src/main/java/com/example/intelliquiz/RetrofitClient.kt
+++ app/src/main/java/com/example/intelliquiz/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.1.2:8000/"  // Replace with your computer's actual IP address
+    private const val BASE_URL = "http://192.168.1.2:8000/"
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
